8a6a77d8a91bb85f14beb268f67b035d998bb6b6
Matthew <70419638+matthewludwick@users.noreply.github.com>
2026-05-20 03:25:21 +0000

    spans and actors

--- js/dom-play.js
+++ js/dom-play.js
@@ -13,3 +13,14 @@
 
 
 */
+
+const spans = document.querySelectorAll("#play span");
+
+console.log(spans);
+
+for (const mySpan of spans){
+    mySpan.style.backgroundColor = "yellow";
+    let actor = mySpan.dataset.actor;
+    alert(actor);
+
+};
